3007a4357e6c955ab48325d8f41b50467026fd43
Alexander Nicholson <alexn400@gmail.com>
2023-08-09 12:30:12 +1200

    Update project settings

--- react-dialog-async/src/DialogProvider.tsx
+++ react-dialog-async/src/DialogProvider.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, useRef } from 'react';
+import { PropsWithChildren, useRef, useState } from 'react';
 import DialogContext, { dialogContextState } from './DialogContext';
 import { DialogComponent } from './types';
 
@@ -9,66 +9,73 @@ interface dialogStateItem {
   resolve?: (value: unknown) => void;
 }
 
-const DialogProvider = ({ children }: PropsWithChildren<{}>) => {
-  const [dialogs, setDialogs] = React.useState<{ [key: string]: dialogStateItem }>({});
+const DialogProvider = ({ children }: PropsWithChildren) => {
+  const [dialogs, setDialogs] = useState<{ [key: string]: dialogStateItem }>({});
   const id = useRef(0);
 
+  /**
+   * Registers a new dialog component and returns the assigned id of that dialog
+   * @param component The component to use
+   * @returns the id of the dialog
+   */
+  const register = (component: DialogComponent<unknown, unknown>): string => {
+    const dialog: dialogStateItem = {
+      component,
+      isOpen: false,
+    };
+
+    const componentId = id.current;
+    id.current++;
+
+    setDialogs((d) => ({ ...d, [componentId]: dialog }));
+    return String(componentId);
+  };
+
+  /**
+   * Removes a registered dialog given its id
+   * @param dialogId the id of the dialog
+   */
+  const unregister = (dialogId: string): void => {
+    setDialogs(({ [dialogId]: _, ...d }) => d);
+  }
+
+      /**
+       * Show the dialog with the given id, and optionally pass in some data. If the dialog is already open, this will just update the data
+       * @param dialogId the id of the dialog
+       * @param data data to pass to the dialog component
+       */
+      const show = (dialogId: string, data: unknown): Promise<unknown> => {
+    return new Promise((resolve) => {
+      setDialogs((d) => ({ ...d, [dialogId]: { ...d[dialogId], data, isOpen: true, resolve } }));
+    });
+  }
+
+  /**
+   * Force closes the dialog with the given id
+   * @param dialogId the id of the dialog
+   */
+  const hide = (dialogId: string): void => {
+    setDialogs((d) => ({ ...d, [dialogId]: { ...d[dialogId], isOpen: false } }));
+  }
+
+  /**
+   * Updates the data of the given dialog
+   * @param dialogId the id of the dialog to update
+   * @param data the new data to pass to the dialog
+   */
+  const updateData= (dialogId: string, data: unknown): void => {
+    setDialogs((d) => ({ ...d, [dialogId]: { ...d[dialogId], data } }));
+  }
+
   const ctx: dialogContextState = {
-    /**
-     * Registers a new dialog component and returns the assigned id of that dialog
-     * @param component The component to use
-     * @returns the id of the dialog
-     */
-    register: (component: DialogComponent<unknown, unknown>): string => {
-      const dialog: dialogStateItem = {
-        component,
-        isOpen: false,
-      };
-
-      const componentId = id.current;
-      id.current++;
-      
-      setDialogs((d) => ({ ...d, [componentId]: dialog }));
-      return String(componentId);
-    },
-
-    /**
-     * Removes a registerd dialog given its id
-     * @param dialogId the id of the dialog
-     */
-    unregister: (dialogId: string): void => {
-      setDialogs(({ [dialogId]: _, ...d }) => d);
-    },
-    /**
-     * Show the dialog with the given id, and optionally pass in some data. If the dialog is already open, this will just update the data
-     * @param dialogId the id of the dialog
-     * @param data data to pass to the dialog component
-     */
-    show: (dialogId: string, data: unknown): Promise<unknown> => {
-      return new Promise((resolve) => {
-        setDialogs((d) => ({ ...d, [dialogId]: { ...d[dialogId], data, isOpen: true, resolve } }));
-      });
-    },
-    /**
-     * Force closes the dialog with the given id
-     * @param dialogId the id of the dialog
-     */
-    hide: (dialogId: string): void => {
-      setDialogs((d) => ({ ...d, [dialogId]: { ...d[dialogId], isOpen: false } }));
-    },
-    
-    /**
-     * Updates the data of the given dialog
-     * @param dialogId the id of the dialog to update
-     * @param data the new data to pass to the dialog
-     */
-    updateData: (dialogId: string, data: unknown): void => {
-      setDialogs((d) => ({ ...d, [dialogId]: { ...d[dialogId], data } }));
-    },
+    register,
+    unregister,
+    show,
+    hide,
+    updateData
   };
 
   return (
-    <>
       <DialogContext.Provider value={ctx}>
         {children}
         {Object.entries(dialogs).map(([id, { component: C, isOpen, data, resolve }]) => {
@@ -83,7 +90,6 @@ const DialogProvider = ({ children }: PropsWithChildren<{}>) => {
           }
         })}
       </DialogContext.Provider>
-    </>
   );
 };
 
