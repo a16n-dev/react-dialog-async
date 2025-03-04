6b01b2e1c3ef460e09fc2e610d2e07dd6147f5ae
Alex Nicholson <alex.n@clove.kitchen>
2025-03-05 10:33:19 +1100

    Fix mounting behaviour

--- packages/react-dialog-async/src/types.ts
+++ packages/react-dialog-async/src/types.ts
@@ -2,6 +2,7 @@ import type { ComponentType } from 'react';
 
 export interface AsyncDialogProps<Request = void, Response = undefined> {
   open: boolean;
+  mounted: boolean;
   handleClose: (data?: Response) => void;
   data: Request;
 }
