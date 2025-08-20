52bf962472f9b2332f4f3051a104ebd09d6492f2
Alex Nicholson <alex.n@clove.kitchen>
2025-08-20 10:27:00 +1000

    Improve how focus state is computed

--- packages/react-dialog-async/src/useRenderDialogs.tsx
+++ packages/react-dialog-async/src/useRenderDialogs.tsx
@@ -5,20 +5,25 @@ const useRenderDialogs = (state: dialogsStateData) => {
   return useMemo(() => {
     const entries = Object.entries(state);
 
-    return entries.map(
-      ([id, { dialog: Component, data, open, resolve }], index) => {
-        return (
-          <Component
-            key={id}
-            open={open}
-            data={data}
-            mounted={true} // Dialog is always mounted when it's being rendered
-            handleClose={(value) => resolve?.(value)}
-            focused={index === entries.length - 1} // Focus the last dialog in the list
-          />
-        );
-      },
-    );
+    // Figure out which dialog has focus.
+    // This isn't super efficient, but should be fine given that having more than 2-3 open dialogs would be very unusual
+    let lastOpenDialog: string;
+    entries.forEach(([id, { open }]) => {
+      if (open) lastOpenDialog = id;
+    });
+
+    return entries.map(([id, { dialog: Component, data, open, resolve }]) => {
+      return (
+        <Component
+          key={id}
+          open={open}
+          data={data}
+          mounted={true} // Dialog is always mounted when it's being rendered
+          handleClose={(value) => resolve?.(value)}
+          focused={id === lastOpenDialog} // Focus the last dialog in the list
+        />
+      );
+    });
   }, [state]);
 };
 
