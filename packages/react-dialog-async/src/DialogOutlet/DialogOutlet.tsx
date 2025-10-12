f6c5f80e5b24fb6936670c6acb7f7e3e950a42e0
Alex Nicholson <alex.n@clove.kitchen>
2025-10-12 17:46:47 +1100

    Fix for real this time

--- packages/react-dialog-async/src/DialogOutlet/DialogOutlet.tsx
+++ packages/react-dialog-async/src/DialogOutlet/DialogOutlet.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useContext, useEffect } from 'react';
 import { GlobalDialogStateContext } from '../context/GlobalDialogStateContext.js';
 import { useRenderDialogs } from './useRenderDialogs.js';
